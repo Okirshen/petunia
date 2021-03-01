5064e98651752ca23e475697f6188cd5dc33b1e2
[HARD]Integral <mohammed.a.tazwar04@gmail.com>
2021-03-01 01:23:50 -0500

    pls commit

--- src/main.py
+++ src/main.py
@@ -15,18 +15,34 @@ async def ping(ctx):
     await ctx.send('Pong!')
 
 @client.command(aliases = ['purge'])
+@commands.has_any_role("Functional Purity", "Elite Hacker")
 async def clear(ctx, amount = 1):
     if amount > 0:
         await ctx.channel.purge(limit = amount + 1)   
-        await ctx.send('Purged ' + str(amount) + ' messages successfully.')
+        await ctx.send(f'Purged {amount} messages successfully.')
 
 @client.command()
+@commands.has_any_role("Functional Purity", "Elite Hacker")
 async def kick(ctx, member : discord.Member, *, reason = None):
     await  member.kick(reason = reason)
 
 @client.command()
+@commands.has_any_role("Functional Purity", "Elite Hacker")
 async def ban(ctx, member : discord.Member, *, reason = None):
     await  member.ban(reason = reason)
+
+@client.command()
+@commands.has_any_role("Functional Purity", "Elite Hacker")
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_disc = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if (user.name, user.discrinator) == (member_name, member_disc):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.name}#{user.discrinator}')
+            return
 
 token = os.environ.get('TOKEN')
 
